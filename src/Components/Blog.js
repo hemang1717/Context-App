d1167a2220e51d7ddefdd2aa05f6c59e4972f33d
hemang1717 <hemang.popli7@gmail.com>
2024-05-31 18:53:37 +0530

    Button Styling changed

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,7 +3,7 @@ import "../App.css";
 import { CounterContext } from "../CounterProvider";
 const Blog = () => {
   const [blogs, setBlogs] = useState([]);
-  const cartItems=useContext(CounterContext);
+  const cartItems = useContext(CounterContext);
 
   function fetchData() {
     fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
@@ -14,7 +14,10 @@ const Blog = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(blogs);
+  //   console.log(blogs);
+  function updateCount(){
+    cartItems.setCount(cartItems.count+1);
+  }
   return (
     <div className="blogComponent">
       <h2>Blog Component</h2>
@@ -24,9 +27,12 @@ const Blog = () => {
             <div key={id} className="blogDetails">
               <div className="eachBlog">
                 <p className="blogTitle">{blog.title}</p>
-                <button onClick={()=>{
-                    cartItems.setCount(cartItems.count+1);
-                    console.log('clicking post of id: ',id)}}>Add to Cart</button>
+                <button
+                  onClick={updateCount}
+                  className="addToCart"
+                >
+                  Add to Cart
+                </button>
               </div>
             </div>
           ))}
